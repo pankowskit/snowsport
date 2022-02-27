a8a38040835d3e38452e1fb67d8d8ec74ad4ca14
pankowskitomasz <pankowski.email@gmail.com>
2022-02-27 15:38:55 +0200

    routing

---
+++ src/views/club.js
@@ -0,0 +1,11 @@
+import {Container} from "react-bootstrap";
+
+function Club(){
+    return(
+        <Container fluid className="minh-footer-adj bg-dark d-flex align-items-center py-5">
+            
+        </Container>
+    );
+}
+
+export default Club;
